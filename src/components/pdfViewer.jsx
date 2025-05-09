02c5a662905cc2674bc62ceec2e527f03af32540
Mohamed Emad <medoeabdo@gmail.com>
2025-05-09 21:55:09 +0300

    Revert "updated resume click"
    This reverts commit 5f56d080c74564dbed91dce49dfc9e6147f03c77.

--- src/components/pdfViewer.jsx
+++ src/components/pdfViewer.jsx
@@ -1,7 +1,7 @@
 const PDFButton = () => {
   return (
     <button 
-      onClick={() => window.open('/MEmad/CV.pdf', '_blank')}
+      onClick={() => window.open('/CV.pdf', '_blank')}
       className="pdf-button"
     >
       Resume <span><i className="fa-solid fa-up-right-from-square"></i></span>
